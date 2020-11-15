938cb24dd2a9b2890e845b1621dc97314e66eb05
RudeBon <yumuema@gmail.com>
2020-11-16 01:23:12 +0200

    rendering users notes from firebase db

--- src/components/Authorized.js
+++ src/components/Authorized.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Divider } from 'antd';
 import 'antd/dist/antd.css';
 import NotesList from './NotesList';
@@ -5,23 +6,40 @@ import AddNoteComp from './AddNoteComp';
 import firebase from 'firebase';
 
 export default function Authorized() {
+    const [userNotes, setUserNotes] = useState({});
+    useEffect(() => {
+        firebase.database().ref('users').child(firebase.auth().currentUser.uid).child('/notes').once('value')
+            .then((data) => {
+                let fetchedData = data.val()
+                console.log('Fetched Data', fetchedData)
+                setUserNotes(data.val())
+                console.log('Data in state', userNotes)
+            })
+            .then()
+            .catch((error) => {
+                console.log('Fetching Error', error)
+            });
+    }, [])
+
+
+
     return (
         <>
             {firebase.auth().currentUser !== null
-            ? (
-                <>
-                    <AddNoteComp />
-                    <Divider />
-                    <NotesList />
-                </>
-            )
-            : (
-                <>
-                    <h1>Unauthorized Access</h1>
-                    <p>You have attemmpted to access a page for which you are not authorized</p>
-                    <p>Please, sign in to use the app.</p>
-                </>
-            )}
+                ? (
+                    <>
+                        <AddNoteComp/>
+                        <Divider />
+                        <NotesList userNotes={userNotes}/>
+                    </>
+                )
+                : (
+                    <>
+                        <h1>Unauthorized Access</h1>
+                        <p>You have attemmpted to access a page for which you are not authorized</p>
+                        <p>Please, sign in to use the app.</p>
+                    </>
+                )}
         </>
     )
 }
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,29 +1,19 @@
 import NoteCard from './NoteCard'
 
-const mockCardsData = [
-    { id: 'hfgdhjdj322b', title: "First Card", text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur" },
-    { id: 'dwjbdskjb322', title: "Another card", text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo" },
-    { id: 'edjhbfdbd23h', title: "Yet another card", text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" },
-    { id: 'hfgdhadj322b', title: "First Card", text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur" },
-    { id: 'dwjbdskj5322', title: "Another card", text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo" },
-    { id: 'e5jhbfdbd23h', title: "Yet another card", text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" },
-]
-
-export default function NotesList() {
-    function getCards() {
-        return mockCardsData.map(note =>
-            <li key={note.id} className="NoteCard">
-                <NoteCard note={note}></NoteCard>
+export default function NotesList(props) {
+    const getUserNotesfromFirebase = () => {
+        return Object.keys(props.userNotes).map(key =>
+            <li key={key} className="NoteCard">
+                <NoteCard note={props.userNotes[key]}></NoteCard>
             </li>
         )
     }
 
-
     return (
         <>
-        <h3>Your notes</h3>
+            <h3>Your notes</h3>
             <ul className="NoteList">
-                {getCards()}
+                {getUserNotesfromFirebase()}
             </ul>
         </>
 
