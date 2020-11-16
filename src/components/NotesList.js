67ca309a5bcd032ff396f9e020c91556f0650ca5
RudeBon <yumuema@gmail.com>
2020-11-17 00:14:30 +0200

    check if there is no user notes

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -2,20 +2,28 @@ import NoteCard from './NoteCard'
 
 export default function NotesList(props) {
     const getUserNotesfromFirebase = () => {
-        return Object.keys(props.userNotes).map(key => 
-            <li key={key} className="NoteCard">
-                <NoteCard note={props.userNotes[key]} noteKey={key}></NoteCard>
-            </li>
-        )
+        if (typeof props.userNotes === 'object' && props.userNotes !== null) {
+            return (
+                <>
+                    <h3>Your notes</h3>
+                    <ul className="NoteList">
+                        {Object.keys(props.userNotes).map(key =>
+                            <li key={key} className="NoteCard">
+                                <NoteCard note={props.userNotes[key]} noteKey={key}></NoteCard>
+                            </li>
+                        )}</ul>
+                </>
+            )
+        } else {
+            return (
+                <h2>Your notes will be displayed here</h2>
+            )
+        }
     }
 
     return (
         <>
-            <h3>Your notes</h3>
-            <ul className="NoteList">
-                {getUserNotesfromFirebase()}
-            </ul>
+            { getUserNotesfromFirebase() }
         </>
-
     )
 }
